01cbaafeda6ef6bd31b0b907a92cba66a457e747
fuku3106 <satoru.tohno@gmail.com>
2020-12-30 18:02:32 +0900

    削除失敗時の動作を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_find, only: [:edit, :show, :update]
+  before_action :item_find, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -35,9 +35,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+       redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
