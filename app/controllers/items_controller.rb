c8837d7bda26ffd3d780473bd7e14075162b7018
fuku3106 <satoru.tohno@gmail.com>
2020-12-30 18:12:45 +0900

    出品したログインユーザーのみ削除できる機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
   before_action :item_find, only: [:edit, :show, :update, :destroy]
 
   def index
@@ -35,7 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if current_user.id == @item.user_id
+       @item.destroy
        redirect_to root_path
     else
       render :show
