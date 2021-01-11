c06cc612c3103b1bff4de098f68085cfa3ea9fc1
fuku3106 <satoru.tohno@gmail.com>
2021-01-11 19:00:26 +0900

    指摘箇所追加、修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,6 +39,6 @@ class OrdersController < ApplicationController
   end
 
   def sold_out_item
-    redirect_to root_path if @item.order.present?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
