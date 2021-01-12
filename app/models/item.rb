978e78ed175d8436b24a96fe4ede1e5bdd18cde2
fuku3106 <satoru.tohno@gmail.com>
2021-01-12 11:14:23 +0900

    商品出品最低価格の訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
 
     validates :price,
               format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true,
-                                                              greater_than: 300, less_than: 10_000_000 }
+                                                              greater_than: 299, less_than: 10_000_000 }
 
     validates :category_id, :condition_id, :days_to_ship_id, :delivery_area_id, :shipping_id, numericality: { other_than: 1 }
   end
